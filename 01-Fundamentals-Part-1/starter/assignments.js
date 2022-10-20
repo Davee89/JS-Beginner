d336aae1b40ae0e1fb2188de631f81116f393bb9
Dawid Matras <davematras@gmail.com>
2022-10-20 19:27:05 +0200

    data types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -2,3 +2,8 @@ let Country = "Poland";
 let Continent = "Europe";
 let Population = "Poles";
 console.log(Country, Continent, Population);
+// * Next
+let isIsland = false;
+let Language;
+console.log(typeof isIsland, typeof Population, typeof Country, typeof Language);
+// * Next
