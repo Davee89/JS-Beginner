601c7af3480eed66d492e94a281e39b988a9cee7
Dawid Matras <davematras@gmail.com>
2022-10-20 19:49:24 +0200

    basic operators\

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,6 +1,6 @@
 let Country = "Poland";
-let Continent = "Europe";
-const Population = "Poles";
+const Continent = "Europe";
+let Population = 38;
 console.log(Country, Continent, Population);
 // * Next
 let isIsland = false;
@@ -9,3 +9,12 @@ console.log(typeof isIsland, typeof Population, typeof Country, typeof Language)
 // * Next
 Language = "polish";
 console.log(Population);
+// * Next
+Population /= 2;
+Population++;
+console.log(Population);
+let PopulationFinland = 6;
+let PopulationAverage = 33;
+let description = "Portugal is in Europe, and its 11 million people speak portuguese";
+console.log(Population > PopulationFinland);
+console.log(Population < PopulationAverage);
