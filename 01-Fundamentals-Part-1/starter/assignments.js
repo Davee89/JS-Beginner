99bc9c583cc51ae0b6c53da5475d176b35ac2085
Dawid Matras <davematras@gmail.com>
2022-10-20 19:35:50 +0200

    variables

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,9 +1,11 @@
 let Country = "Poland";
 let Continent = "Europe";
-let Population = "Poles";
+const Population = "Poles";
 console.log(Country, Continent, Population);
 // * Next
 let isIsland = false;
 let Language;
 console.log(typeof isIsland, typeof Population, typeof Country, typeof Language);
 // * Next
+Language = "polish";
+console.log(Population);
