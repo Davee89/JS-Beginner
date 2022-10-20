a9a00be6c01a7c3471f2d379c7d9d947afd1c180
Dawid Matras <davematras@gmail.com>
2022-10-20 21:58:21 +0200

    1st challenge

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -18,3 +18,23 @@ let PopulationAverage = 33;
 let description = "Portugal is in Europe, and its 11 million people speak portuguese";
 console.log(Population > PopulationFinland);
 console.log(Population < PopulationAverage);
+// ! 1st challenge ! //
+let massMark = 78;
+let massJohn = 92;
+let heightMark = 1.69;
+let heightJohn = 1.95;
+let MarkBMI = massMark / heightMark ** 2;
+let JohnBMI = massJohn / heightJohn ** 2;
+console.log(MarkBMI, JohnBMI);
+let markHigherBMI = MarkBMI > JohnBMI;
+console.log(markHigherBMI);
+massMark = 95;
+massJohn = 85;
+heightMark = 1.88;
+heightJohn = 1.76;
+MarkBMI = massMark / heightMark ** 2;
+JohnBMI = massJohn / heightJohn ** 2;
+console.log(MarkBMI, JohnBMI);
+markHigherBMI = MarkBMI > JohnBMI;
+console.log(markHigherBMI);
+// * Next
