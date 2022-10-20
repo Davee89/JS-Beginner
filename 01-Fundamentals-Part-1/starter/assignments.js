395fcd655a3d08f078c9aa09beddd40531be28eb
Dawid Matras <davematras@gmail.com>
2022-10-20 18:58:32 +0200

    variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,4 @@
+let Country = "Poland";
+let Continent = "Europe";
+let Population = "Poles";
+console.log(Country, Continent, Population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,6 @@
 let js = "amazing";
+let firstName = "Jonas";
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Coordinator";
