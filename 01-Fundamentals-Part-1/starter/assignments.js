834c7cb219613c8494cedde67b48b9aa3631f77a
Dawid Matras <davematras@gmail.com>
2022-10-21 18:39:52 +0200

    equality operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -53,3 +53,11 @@ if (MarkBMI > JohnBMI) {
   console.log(`John's BMI (${Math.round(JohnBMI)}) is higher than Mark's (${Math.round(MarkBMI)})!`);
 }
 // * Next
+const numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+if (numNeighbours === 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
