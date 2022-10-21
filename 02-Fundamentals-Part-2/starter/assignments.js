eea2b7ebc2d62c4244bdb4fdb12de42054ea909a
Dawid Matras <davematras@gmail.com>
2022-10-21 23:39:28 +0200

    arrow function

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -6,4 +6,26 @@ const Poland = describeCountry("Poland", 38, "Warsaw");
 const Germany = describeCountry("Germany", 80, "Berlin");
 const USA = describeCountry("USA", 120, "Washington");
 console.log(Poland, Germany, USA);
+
+// * NEXT
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percentagePoland = percentageOfWorld1(38);
+const percentageGermany = percentageOfWorld1(80);
+const percentageUSA = percentageOfWorld1(120);
+console.log(percentagePoland, percentageGermany, percentageUSA);
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+console.log(percentageOfWorld2(38), percentageOfWorld2(80), percentageOfWorld2(120));
+
+// * NEXT
+
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+console.log(percentageOfWorld3(38), percentageOfWorld3(80), percentageOfWorld3(120));
+
 // * NEXT
