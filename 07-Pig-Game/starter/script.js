e069f5447e4e8e17be50fbcdc8d1ae6a475202ad
Dawid Matras <davematras@gmail.com>
2022-10-27 22:32:17 +0200

    added comments

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,43 +3,65 @@ let rollNumber = Math.trunc(Math.random() * 6) + 1;
 let current = 0;
 let total = [0, 0];
 let activePlayer = 0;
+let playing = true;
 const dice = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const btnNew = document.querySelector('.btn--new');
+
 // * Zeroing the current score!
 const zeroingCurrent = function () {
   current = 0;
   document.querySelector(`#current--${activePlayer}`).textContent = 0;
 };
-
-btnRoll.addEventListener('click', function () {
-  rollNumber = Math.trunc(Math.random() * 6) + 1;
-  dice.setAttribute('src', `dice-${rollNumber}.png`);
-  if (rollNumber !== 1) {
-    current += rollNumber;
-    document.querySelector(`#current--${activePlayer}`).textContent = current;
-  } else {
-    zeroingCurrent();
-    activePlayer = activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    document.querySelector(`.player--0`).classList.toggle('player--active');
-    document.querySelector(`.player--1`).classList.toggle('player--active');
-  }
-});
-
-btnHold.addEventListener('click', function () {
-  total[`${activePlayer}`] += current;
-  document.querySelector(`#score--${activePlayer}`).textContent =
-    total[`${activePlayer}`];
+// * Switching players
+const switchPlayer = function () {
+  activePlayer = activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
   document.querySelector(`.player--0`).classList.toggle('player--active');
   document.querySelector(`.player--1`).classList.toggle('player--active');
-  if (total[`${activePlayer}`] >= 100) {
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--winner');
-    // document
-    //   .querySelector(`.name--${activePlayer}`)
-    //   .classList.add('player--winner name');
+};
+
+// ! Button ROLL ! //
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    //   Rolling and showing dice
+    rollNumber = Math.trunc(Math.random() * 6) + 1;
+    dice.setAttribute('src', `dice-${rollNumber}.png`);
+    //   Adding score to current count
+    if (rollNumber !== 1) {
+      current += rollNumber;
+      document.querySelector(`#current--${activePlayer}`).textContent = current;
+    }
+    //   If getting 1 on dice swaping to other player
+    else {
+      zeroingCurrent();
+      switchPlayer();
+    }
   }
-  zeroingCurrent();
-  activePlayer = activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
 });
+// ! BUTTON HOLD ! //
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //   Adding current score to total
+    total[`${activePlayer}`] += current;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      total[`${activePlayer}`];
+    zeroingCurrent();
+    //   Winning condition
+    if (total[`${activePlayer}`] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      dice.style.display = 'none';
+      // Swaping players if still not won.
+    } else {
+      switchPlayer();
+    }
+  }
+});
+// ! BUTTON NEW GAME ! //
+btnNew.addEventListener('click', function () {});
