e974a61598fbe6cf9afbd6947137451ec7ca35e7
Dawid Matras <davematras@gmail.com>
2022-11-10 22:07:16 +0100

    1st

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const bookings = [];
+function createBooking(flightNum = none, numPassengers = 1, price = 200) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+}
+
+createBooking('Lh123', 100);
