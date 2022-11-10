66abedff78de4e8ba32d0646e0c7bb8e857953d2
Dawid Matras <davematras@gmail.com>
2022-11-10 22:34:17 +0100

    2nd

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -12,3 +12,6 @@ function createBooking(flightNum = none, numPassengers = 1, price = 200) {
 }
 
 createBooking('Lh123', 100);
+
+const greet = greeting => name => console.log(`${greeting} ${name}`);
+greet('Hello')('David');
