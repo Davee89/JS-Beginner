e7819e45f2fcb607e3ba09bf60151c6631118941
Dawid Matras <davematras@gmail.com>
2022-11-12 14:51:46 +0100

    challenge 2 completed

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -132,3 +132,14 @@ createUserName(accounts); // stw
 
 // const balance = movements.reduce((acc, cur, i, arr) => acc + cur, 0);
 // console.log(balance);
+
+// ! CHALLENGE 2 ! //
+const calcAverageHumanAge = function (ages) {
+  const humanAverage = ages
+    .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  return humanAverage;
+};
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
